f36cf430ff11e9885200844f63219cef9e539854
Mike McCall <pyramind10@gmail.com>
2016-08-24 23:44:04 -0700

    ch.8 up to page 145

---
+++ test/controllers/auth_test.exs
@@ -0,0 +1,33 @@
+defmodule Rumbl.AuthTest do
+  use Rumbl.ConnCase
+  alias Rumbl.Auth
+
+  setup %{conn: conn} do
+    conn =
+      conn
+      |> bypass_through(Rumbl.Router, :browser)
+      |> get("/")
+      {:ok, %{conn: conn}}
+  end
+
+  test "authenticate_user halts when no current_user exists", %{conn: conn} do
+    conn = Auth.authenticate_user(conn, [])
+    assert conn.halted
+  end
+  test "authenticate_user continues when the current_user exists", %{conn: conn} do
+    conn =
+      conn
+      |> assign(:current_user, %Rumbl.User{}) |> Auth.authenticate_user([])
+    refute conn.halted
+  end
+
+  test "login puts the user in the session", %{conn: conn} do
+    login_conn =
+      conn
+      |> Auth.login(%Rumbl.User{id: 124})
+      |> send_resp(:ok, "")
+
+    next_conn = get(login_conn, "/")
+    assert get_session(next_conn, :user_id) == 124
+  end
+end
